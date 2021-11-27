89cbac0a8b4278ddddc9259c124f7cd40fb89485
Alexei Jennings <alex.jennings@sclhealth.org>
2021-11-27 15:08:27 -0700

    Embedding database

---
+++ ADIF.NET/Tags/HrdLogQSOUploadStatus.cs
@@ -0,0 +1,12 @@
+﻿using ADIF.NET.Attributes;
+
+namespace ADIF.NET.Tags {
+
+  public class HrdLogQSOUploadStatus : RestrictedEnumerationTag, ITag {
+
+    public override string Name => TagNames.HrdLogQSOUploadStatus;
+
+    [DefaultValue("N")]
+    public override string[] Options => Values.QSOUploadStatuses.GetOptions();
+    }
+  }
